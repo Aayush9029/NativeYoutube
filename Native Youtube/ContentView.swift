6ce1af6441bfc3d253c587606235a5ff213273cf
Aayush9029 <aayushpokharel36@gmail.com>
2021-05-14 21:42:59 -0400

    Initial Commit

---
+++ Native Youtube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Native Youtube
+//
+//  Created by Aayush Pokharel on 2021-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Native Youtube/Native_YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Native_YoutubeApp.swift
+//  Native Youtube
+//
+//  Created by Aayush Pokharel on 2021-05-14.
+//
+
+import SwiftUI
+
+@main
+struct Native_YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
