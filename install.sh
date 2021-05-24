6aa4c7a5091f921d013582957995c384af94f3dd
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-05-24 08:00:59 +0530

    New install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,23 @@
-#!/bin/bash
+
 
 echo "Installing / Updating Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing / Updating mpv and youtube dl"
+brew install wget
 brew install mpv youtube-dl
 brew link youtube-dl
 brew link mpv
 
-echo "Downloading..."
-wget https://github.com/Aayush9029/Native-Youtube/releases/download/v0.02/Muubii.app.zip > /dev/null 2>&1
-
-echo "Unzipping the file"
+echo "Downloading...."
+wget https://github.com/Aayush9029/Native-Youtube/releases/download/v0.02/Muubii.app.zip
+echo "Installing Muubbi"
+ls
 unzip -qq Muubii.app.zip
 
-echo "removing cache"
-rm Muubii.app.zip
-
-echo "Saving to your Applications folder"
+echo "Finishing up..."
+rm -rf Muubii.app.zip
 mv Muubii.app /Applications
 
-echo "DONE :)"
+echo "Successfully installed Muubbi! Check your applications!"
 
-echo "Exiting terminal in 3"
-sleep 2
-echo "2"
-sleep 1
-echo "1"
-sleep 1
-exit
