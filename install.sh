9b78bf76c8204d0beec3b666f9112dd49e95684c
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-05-25 12:29:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 
-echo "Installing / Updating Homebrew"
+echo "Checking /Installing"
+if ! type brew > /dev/null;
+then
+echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo "Installing / Updating mpv and youtube dl"
-brew install mpv youtube-dl
-brew link youtube-dl
+fi
+echo "Installing / Checking dependencies"
+if brew ls --versions mpv && brew ls --versions youtube-dl > /dev/null;
+then
+echo "Not installing because the packages already exist"
+else
+echo "Installing Dependencies"
+brew install mpv
+brew install youtube-dl
 brew link mpv
+brew link youtube-dl
+
+fi
 
 echo "Downloading..."
 cd /Applications
