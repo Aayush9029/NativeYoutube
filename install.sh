1be2bdc2539f88570b6b7b505f1bf210c97756f3
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-05-25 08:33:20 +0530

    Reverted back to wget
    Why revert? Well because curl is fetching the file in a manner in which 'unzip' cannot read, it is apparently an 'Unsupported file format'. Thus are my reasons of reverting BACK to wget because cURL fetches it in a manner where 'unzip' is unable to unzip.

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ echo "Installing / Updating Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing / Updating mpv and youtube dl"
-brew install mpv youtube-dl
+brew install mpv youtube-dl wget
 brew link youtube-dl
 brew link mpv
-
+brew link wget
 
 echo "Downloading..."
 cd /Applications
-curl -o https://github.com/Aayush9029/Native-Youtube/releases/download/v0.02/Muubii.app.zip
+wget https://github.com/Aayush9029/Native-Youtube/releases/download/v0.02/Muubii.app.zip > /dev/null 2>&1
 
 echo "Installing"
 unzip -qq Muubii.app.zip
