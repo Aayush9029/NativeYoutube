c6b0b30bd8ae9ac9940928c8ff6c7351190ef0b3
CorsairRGB 16gb DDR4 3200MHz RAM <69950985+insomnia-creator@users.noreply.github.com>
2021-05-26 12:08:59 +0530

    iNsTaLl.sH

--- install.sh
+++ install.sh
@@ -1,21 +1,15 @@
-CYAN_IS_SUS="\033[0;36m"
-RED="\033[0;31m"
-YELLOW="\033[0;33m"
-PURPLE="\033[0;35m"
-GREEN="\033[0;32m"
-
-echo "${YELLOW}Checking / ${RED}Installing"
+echo "Checking /}Installing"
 if ! type brew > /dev/null;
 then
-echo "${CYAN_IS_SUS}Installing ${GREEN}Homebrew..."
+echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-echo "${PURPLE}Installing / ${YELLOW}Checking dependencies"
+echo "Installing / Checking dependencies"
 if brew ls --versions mpv && brew ls --versions youtube-dl > /dev/null;
 then
-echo "${GREEN}Not installing because the packages already exist"
+echo "Not installing because the packages already exist"
 else
-echo "${CYAN_IS_SUS}Installing Dependencies"
+echo "Installing Dependencies"
 brew install mpv
 brew install youtube-dl
 brew link mpv
@@ -23,22 +17,25 @@ brew link youtube-dl
 
 fi
 
-echo "${YELLOW}Downloading..."
+echo "Downloading..."
 cd /Applications
 curl -LO https://github.com/Aayush9029/Native-Youtube/releases/download/v0.02/Muubii.app.zip
 
-echo "${GREEN}Installing"
+echo "Installing"
 unzip -qq Muubii.app.zip
 rm Muubii.app.zip
 xattr -dr com.apple.quarantine Muubii.app
 echo "Removing quarantine"
 
-echo "${PURPLE}Installed"
+echo "Installed"
 
-echo "${CYAN_IS_SUS}Exiting terminal in 3"
+open Muubii.app
+echo "Exiting terminal in 3 seconds"
 sleep 1
-echo "${RED}2"
-sleep 1
-echo "${YELLOW}1"
+
+echo "Exiting terminal in 2 seconds"
+sleep 1 
+echo "Exiting terminal in 1 second"
+
 sleep 1
 exit
